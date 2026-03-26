a08693e34c75cf7a491e767f158a31fdb4d3650a
AFS0026652 <yehormaksiuchenko@gmail.com>
2026-03-26 11:43:07 +0100

    Properly setup Tailiwnd v4
    Fix dependencies and all the problems with tailwindCSS
    
    Document the future workflow in globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    
-  
+    <div>
+      <div>Hello World</div>
+      <p className="text-primary">This is pink</p>
+    </div>
+  )
 }
