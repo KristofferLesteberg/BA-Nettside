2dc9e58dbc776fc9c6e1e4e3bc21428f80275375
AFS0026652 <yehormaksiuchenko@gmail.com>
2026-04-09 12:26:59 +0200

    Fix merge
    Also delete the test-images API route.

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-
-//Initilaze PrismaClient - and handling multiply connections to the database
-
-import { PrismaClient } from "../../generated/prisma";
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-=======
 import { PrismaClient } from "../../generated/prisma"
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
 
@@ -25,4 +13,3 @@ function createPrismaClient() {
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
->>>>>>> database
