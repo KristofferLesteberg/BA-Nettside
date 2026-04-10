bf08dc2fbcbd5165d44c8a09a4b7e3ca620d8997
Yehor Maksiuchenko <yehormaksiuchenko@gmail.com>
2026-04-10 02:38:14 +0200

    Add ImageCarousel component
    Based on the Swiper, this component can be used to view any set of images across the web-site.

---
+++ app/components/shared/ImageCarousel.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+"use client"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+export default function Carousel({ images, className }: { images: string[], className?: string}) {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className={`mySwiper ${className ?? ''}`}
+    >
+      {images.map((imageId) => (
+        <SwiperSlide key={imageId} className="flex items-center justify-center">
+          <img 
+            src={`/images/${imageId}.webp`}
+            alt=""
+            className="w-full h-full object-contain"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
