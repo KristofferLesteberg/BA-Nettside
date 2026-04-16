b07ca87c916fa473c8fd862b5546f66042e9ed2e
Yehor Maksiuchenko <yehormaksiuchenko@gmail.com>
2026-04-17 01:54:24 +0200

    Improve MeasurementList UI/UX
    Visually define each input group as one whole object, while separating them between each other.

--- app/components/admin/MeasurementInput.tsx
+++ app/components/admin/MeasurementInput.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function MeasurementInput({ name, value, onChange, onDelete }: Props) {
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row gap-2 bg-surface border-2 border-default rounded-md p-1">
       <input type="text" placeholder="Navn" className="input" value={name} onChange={e => onChange("name", e.target.value)} />
       <input type="text" placeholder="Verdi" className="input" value={value} onChange={e => onChange("value", e.target.value)} />
       <button type="button" className="btn btn-error btn-icon self-center shrink-0" onClick={onDelete}>
