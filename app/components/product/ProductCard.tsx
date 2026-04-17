92d741a3d6089cf4d4f1f5f09c7703b6d64866cd
Kristoffer <Kristofferihmelesteberg@gmail.com>
2026-04-17 23:24:44 +0200

    Added so when the product is pressed the user is sent to the products/id page

--- app/components/product/ProductCard.tsx
+++ app/components/product/ProductCard.tsx
@@ -16,25 +16,28 @@ const ProductCard = ({ product, isAdmin }: ProductCardProps ) => {
     
 
   return (
-    <div className="rounded w-100 h-full">
-        {product.images[0] && (
-            <img src={`/images/${product.images[0].id}.webp`} className="w-full h-70 rounded" />
-        )}
-        <h1 className="heading-4 text-red-500 mt-5">{product.title}</h1>
-        <br />
-        {`${Number(product.price)}kr`}
-        <br />
-        {product.publishedAt}
-        {isAdmin && (
-            <div>
-                <DeleteProduct productID={product.id}/>
-                <Link href={`/admin/updateProduct/${product.id}`}>
-                    <button className="btn btn-outline">Edit</button>
-                </Link>
-              
-            </div>
-        )}
-    </div>
+        <div className="rounded w-100 h-full border shadow-sm transition-all duration-300 cursor-pointer hover:translate-y-1 hover:shadow-lg">
+            <Link href={`/products/${product.id}`}>
+            {product.images[0] && (
+                <img src={`/images/${product.images[0].id}.webp`} className="w-full h-70 rounded" />
+            )}
+            <h1 className="heading-4 text-red-500 mt-5">{product.title}</h1>
+            <br />
+            {`${Number(product.price)}kr`}
+            <br />
+            {product.publishedAt}
+            </Link>
+            {isAdmin && (
+                <div className="">
+                    <DeleteProduct productID={product.id}/>
+                    <Link href={`/admin/updateProduct/${product.id}`}>
+                        <button className="btn btn-outline">Edit</button>
+                    </Link>
+                
+                </div>
+            )}
+        </div>
+
   )
 }
 
