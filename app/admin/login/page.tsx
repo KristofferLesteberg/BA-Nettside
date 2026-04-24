f969f6a927814d6ec0a124383815e16a57fad2ef
AFS0026652 <yehormaksiuchenko@gmail.com>
2026-04-24 03:22:46 +0200

    Get rid of LoginForm component

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,15 +1,48 @@
+"use client";
 
-import React from 'react'
-import { LoginForm } from '../../components/admin/LoginForm'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
+export default function LoginForm () {
 
+  const router = useRouter()
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  //const {error, setError} = useState("")
 
-const page = () => {
+  const handleLogin = async (e: any) => {
+    e.preventDefault()
+
+    const result = await signIn("credentials", {
+      username: username,
+      password: password,
+      redirect: false
+    })
+    if(result?.error) {
+      console.log("error")
+      //setError("Feil passord eller brukernavn")
+    } else {
+      router.push('/admin')
+    }
+  }
+
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <form onSubmit={handleLogin}>
+      <input
+        type="text"
+        placeholder='brukernavn'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder='passord'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+     
+      <button type='submit'>log in</button>
+    </form>
   )
 }
-
-export default page
