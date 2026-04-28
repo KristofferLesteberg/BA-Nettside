b814a336d7153d66c748ac74520c55930ca6cc22
Kristoffer <Kristofferihmelesteberg@gmail.com>
2026-04-28 12:16:54 +0200

    Added the new structure

--- app/products/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1>Product Not Found</h1>
-      <p>The product you are looking for does not seem to exist.</p>
-    </div>
-  )
-}
