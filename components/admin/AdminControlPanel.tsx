740d1baca1021ec1c9470a02c164b31197c33859
Yehor Maksiuchenko <yehormaksiuchenko@gmail.com>
2026-05-16 16:28:47 +0200

    Enhance the Log Out button
    Add confirmation popup and an icon to better convey this button's purpose

--- components/admin/AdminControlPanel.tsx
+++ components/admin/AdminControlPanel.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import { signOut } from "next-auth/react"
+import { usePopUp } from "../shared/PopUp"
+import { FaSignOutAlt } from "react-icons/fa";
 
 export default function AdminControlPanel() {
+  const { open: openPopUp, element: popUpElement } = usePopUp()
+
   return (
     <div className="flex items-center gap-2">
+      {popUpElement}
       <button
-        onClick={() => signOut({ callbackUrl: "/admin/login" })}
-        className="btn btn-error text-xs py-1.5"
+        onClick={() => openPopUp({
+          title: "Logg ut?",
+          subtitle: "Er du sikker på at du vil logge ut?",
+          yesLabel: "Ja, logg ut",
+          noLabel: "Nei, bli værende",
+          onYes: () => signOut({ callbackUrl: '/' })
+        })}
+        className="btn btn-error py-1.5"
       >
+        <FaSignOutAlt className="mr-1"/>
         Logg ut
       </button>
     </div>
