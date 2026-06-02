6ed5458eab68207576dcc61437edc7f42bf7e21f
AFS0026652 <yehormaksiuchenko@gmail.com>
2026-06-03 00:11:50 +0200

    Fix attempt: images handling in prod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
-# Fix ownership of the bind-mounted data directory, then drop to the app user
 chown -R ba-user:ba-group /app/data
+mkdir -p /app/public/images/high-res /app/public/images/med-res /app/public/images/low-res
+chown -R ba-user:ba-group /app/public/images
 exec su-exec ba-user "$@"
