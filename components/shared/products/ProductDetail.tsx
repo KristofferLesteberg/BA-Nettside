d968261a310785c5e98a6ded93b385b29305e8cd
AFS0026652 <yehormaksiuchenko@gmail.com>
2026-06-02 10:07:42 +0200

    Widen the product page

--- components/shared/products/ProductDetail.tsx
+++ components/shared/products/ProductDetail.tsx
@@ -46,7 +46,7 @@ export default function ProductDetail({ product }: { product: Product }) {
 
   return (
     <div className="bg-page min-h-screen">
-      <div className="max-w-5xl mx-auto px-4 py-6 sm:px-6 sm:py-8">
+      <div className="max-w-6xl mx-auto px-4 py-6 sm:px-6 sm:py-8 lg:px-4">
 
         {/* Main layout */}
         <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
