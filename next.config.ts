2da96074a00356126e32ee815135c217cfdf766b
Yehor Maksiuchenko <yehormaksiuchenko@gmail.com>
2026-06-16 18:32:25 +0200

    Enable the Image optimization back again

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
   serverExternalPackages: ['@react-pdf/renderer'],
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: "20mb",
